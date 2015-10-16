8699dd4f96881768289f053d501117887d43f827
Bob Chatman <bob.chatman@gmail.com>
2015-10-16 14:43:42 -0700

    Returning numbers

--- src/FlatheadCore/Private/Types/Translate.cpp
+++ src/FlatheadCore/Private/Types/Translate.cpp
@@ -7,6 +7,7 @@
 #include "Impl/BooleanIMPL.h"
 #include "Impl/PrimitiveIMPL.h"
 #include "Impl/StringIMPL.h"
+#include "Impl/NumberIMPL.h"
 
 using namespace v8;
 using namespace Gneu::Types;
@@ -21,6 +22,11 @@ Gneu::Types::Value *Translate::ToFlathead(v8::Handle<v8::Value> v8Value)
 	if (v8Value->IsString())
 	{
 		return new Gneu::Types::StringIMPL(v8Value);
+	}
+
+	if (v8Value->IsNumber())
+	{
+		return new Gneu::Types::NumberIMPL(v8Value);
 	}
 
 	return new Gneu::Types::PrimitiveIMPL(v8Value);
